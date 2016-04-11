238ce624b27b23337cd7389fd11532a358d4c5fa
Angus Wu <awu@squiz.net>
2016-04-11 21:50:53 +1000

    modified global js

--- js/global.js
+++ js/global.js
@@ -35,27 +35,37 @@
     return total.toFixed(2);
   }
 
-  function generateAuPDF(){
+  function createPDF(currency, theme){
     // create the pdf object
     var doc = new jsPDF('p','pt');
     // get date
     var date = new Date();
+    // print date format
     var printDate = 'Order Date: ' + date.getDay() + '/' + date.getMonth() + '/' + date.getFullYear();
     // get tracking number
     var tracking = $('#tracking-number').val();
     // get delivery cost
-    var delivery = parseFloat( $('#delivery-cost').val() ).toFixed(2);
-
+    var delivery = ( currency == 'RMB' ) ? ( parseFloat( $('#delivery-cost').val() ) * 6 ).toFixed(2) : parseFloat( $('#delivery-cost').val() ).toFixed(2);
 
     doc.text(20,20,printDate); // date
     doc.text(20,40,'Tracking Number:   ' + tracking); // tracking number
     doc.text(20,60,'Delivery Cost:   ' + delivery); // delivery cost
-    doc.text(20,80,'Currency:   AUD');
-
+    doc.text(20,80,'Currency:   ' + currency);
 
     var res = doc.autoTableHtmlToJson(document.getElementsByTagName("table")[0]);
+
+    if( currency == 'RMB'){
+      // reset the unit price to RMB 
+      for(var i = 0; i < res.data.length - 1; i++){
+        res.data[i][2] = (parseFloat(res.data[i][2]) * 6).toFixed(2);
+        res.data[i][3] = ( parseInt(res.data[i][1]) * res.data[i][2] ).toFixed(2);
+      }
+      // reset the total cost to RMB
+      res.data[res.data.length - 1][res.data[res.data.length - 1].length -1] = ( calculateTotalCost() * 6 ).toFixed(2);
+    } 
+
     doc.autoTable(res.columns, res.data, {
-      theme: 'striped',
+      theme: theme,
       startY: 120,
       styles: {
         overflow: 'linebreak',
@@ -72,50 +82,14 @@
     doc.save( tracking + '.pdf');
   }
 
+  function generateAuPDF(){
+    // create PDF
+    createPDF( 'AUD', 'striped' );
+  }
+
   function generateRmbPDF(){
-    // create the pdf object
-    var doc = new jsPDF('p','pt');
-    // get date
-    var date = new Date();
-    var printDate = 'Order Date: ' + date.getDay() + '/' + date.getMonth() + '/' + date.getFullYear();
-    // get tracking number
-    var tracking = $('#tracking-number').val();
-    // get delivery cost
-    var delivery = ( parseFloat( $('#delivery-cost').val() ) * 6 ).toFixed(2);
-
-
-    doc.text(20,20,printDate); // date
-    doc.text(20,40,'Tracking Number:   ' + tracking); // tracking number
-    doc.text(20,60,'Delivery Cost:   ' + delivery); // delivery cost
-    doc.text(20,80,'Currency:   RMB');
-
-    var res = doc.autoTableHtmlToJson(document.getElementsByTagName("table")[0]);
-
-    // reset the unit price to RMB 
-    for(var i = 0; i < res.data.length - 1; i++){
-      res.data[i][2] = (parseFloat(res.data[i][2]) * 6).toFixed(2);
-      res.data[i][3] = ( parseInt(res.data[i][1]) * res.data[i][2] ).toFixed(2);
-    }
-
-    // reset the total cost to RMB
-    res.data[res.data.length - 1][res.data[res.data.length - 1].length -1] = ( calculateTotalCost() * 6 ).toFixed(2);
-
-    doc.autoTable(res.columns, res.data, {
-      theme: 'grid',
-      startY: 120,
-      styles: {
-        overflow: 'linebreak',
-        fontSize: 16,
-        rowHeight: 30,
-        halign: 'left',
-        valign: 'middle'
-      },
-      columnStyles: {
-        1: {columnWidth: 'auto'}
-      }
-    });
-
-    doc.save( tracking + '.pdf');
+    // create PDF
+    createPDF( 'RMB', 'grid' );
   }
 
 /*
